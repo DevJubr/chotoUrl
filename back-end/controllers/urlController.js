955b3870b7218e4bfa953b11c6d2b3f50a5bbdad
Jr <jubayerrahman203@gmail.com>
2023-07-13 22:51:53 +0600

    df

--- back-end/controllers/urlController.js
+++ back-end/controllers/urlController.js
@@ -1,7 +1,21 @@
-const urlController = (req, res) => {
-  const { body } = req;
-  res.json({ body });
-  console.log(body);
+const { format } = require("date-fns");
+const Url = require("../models/Url");
+const { generate } = require("shortid");
+const urlController = async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    res.status(400).json({ message: "url is undifind" });
+  }
+
+  const newEntry = new Url({
+    longUrl: url,
+    shortUrl: generate(),
+    history: format(new Date(), "HH:mm:ss - dd/MM/yyyy"),
+  });
+  await newEntry.save();
+
+  res.status(200).json({ message: newEntry });
 };
 
 module.exports = urlController;
