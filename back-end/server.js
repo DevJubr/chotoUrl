756f75dc8db3c479642799053db36800424d2cb5
Jr <jubayerrahman203@gmail.com>
2023-07-15 08:29:07 +0600

    fd

--- back-end/server.js
+++ back-end/server.js
@@ -18,8 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.post("/api/v1/url", urlController);
 app.patch("/api/v1/urlupdate", updateUrlController);
 app.get("/:url", redirectController);
-app.get("/api/v1/allurls", getAllUrlsController);
 app.delete("/api/v1/url/:id", deleteUrlController);
+app.get("/api/v1/allurls", getAllUrlsController);
+app.get("/", (_req, res) => {
+  res.send("working fine");
+});
 
 //PORT
 const PORT = 8000;
@@ -28,3 +31,5 @@ app.listen(PORT, () => {
   dbConnection();
   console.log(`server listening on PORT ${PORT}`);
 });
+
+module.exports = app;
