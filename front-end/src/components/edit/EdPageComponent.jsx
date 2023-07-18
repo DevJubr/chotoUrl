ade5f2fdc3f0373f2cb60b149f08658ccbe28e7f
Jr <jubayerrahman203@gmail.com>
2023-07-18 15:40:35 +0600

    new build

--- front-end/src/components/edit/EdPageComponent.jsx
+++ front-end/src/components/edit/EdPageComponent.jsx
@@ -37,7 +37,7 @@ const EdPageComponent = () => {
   if (loader) {
     return <LoaderCon>Loading...</LoaderCon>;
   }
-  if (data === null || !data?.urls) {
+  if (data === null || data?.urls.length === 0) {
     return (
       <Specer>
         <SmallHeader title={" DB is MT."} />
